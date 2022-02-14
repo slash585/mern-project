b43cf16c8ed3e03abadaabff425aa6a5457d7190
Mehmet Ali Özdemir <mehmetali@192.168.1.87>
2022-02-15 02:11:29 +0300

    Create passport and GoogleStrategy infrastructure

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const app = express();
+const express = require("express")
+const passport = require("passport")
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const keys = require("./config/keys")
+const app = express()
 
-app.get('/', (req,res) => {
-    res.send({hi: "there"})
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClienSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Access token:" , accessToken)
+      console.log("Refresh token:" , refreshToken)
+      console.log("Profile token:" , profile)
+    }
+  )
+)
 
-app.listen(3000, ()=>{
-    console.log('Server started listening to 3000')
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}))
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+app.listen(5001, () => {
+  console.log("Server started listening to 3000")
 })
