37a3376fb8102742288d4176cfdc9d322b7ca34d
slash585 <slsh585@gmail.com>
2022-02-16 02:52:13 +0300

    Implement basic react-router structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
+  const Header = () => <h2>Header</h2>
+  const Dashboard = () => <h2>Dashboard</h2>
+  const SurveyNew = () => <h2>SurveyNew</h2>
+  const Landing = () => <h2>Landing</h2>
+
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
