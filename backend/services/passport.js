2d8024e03777935222b1fd9d4f17c88286e20609
slash585 <slsh585@gmail.com>
2022-02-16 02:22:38 +0300

    Convert user login and registration processes to async

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -21,18 +21,14 @@ module.exports = passport.use(
       clientSecret: keys.googleClienSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken, refreshToken, profile, done) => {
-      const user = User.findOne({ googleId: profile.id }).then(
-        (existingUser) => {
-          if (existingUser) {
-            done(null, existingUser)
-          } else {
-            new User({ googleId: profile.id })
-              .save()
-              .then((user) => done(null, user))
-          }
-        }
-      )
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+      if (existingUser) {
+        return done(null, existingUser)
+      }
+      
+      const newUser = await new User({ googleId: profile.id }).save()
+      done(null, newUser)
     }
   )
 )
